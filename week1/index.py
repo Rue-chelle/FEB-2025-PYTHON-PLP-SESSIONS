8018e81e34e4126f1ec797d559f4da99f7d941f9
Rue-chelle <misshie21@gmail.com>
2025-03-13 12:01:31 +0200

    Calculator

--- week1/index.py
+++ week1/index.py
@@ -1,1 +1,26 @@
-print("hello world")
+num1 = float(input("insert a number: "))
+num2 = float(input("insert the second number: "))
+operation =input(" Enter an operation (+,-,*,/)")
+
+if operation == '+' :
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result =  num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*" :
+    result = num1 * num2 
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/' :
+ if num2 !=0 :
+     result = num1 / num2
+     print(f"{num1} / {num2} = {result}")
+
+ else: print("Invalid: Divivion by 0 is not allowed.")
+
+else:
+    result =str("please enter a valid operation")
+
+    print(result)
+
+
